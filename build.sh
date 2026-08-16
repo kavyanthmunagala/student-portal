793eb54ec45a2758d08b762a558f9f9ce2e78c8b
techteam-venturemond <techteam@ventuemond.com>
2026-08-17 00:06:57 +0530

    Make build compiler configurable

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e
 
+CXX=${CXX:-g++}
+
 rm -f student-portal
 
-g++ -std=c++17 main.cpp StudentManager.cpp StudentSearch.cpp AttendanceValidator.cpp Login.cpp Profile.cpp Dashboard.cpp Settings.cpp -o student-portal
+$CXX -std=c++17 \
+    main.cpp \
+    StudentManager.cpp \
+    StudentSearch.cpp \
+    AttendanceValidator.cpp \
+    Login.cpp \
+    Profile.cpp \
+    Dashboard.cpp \
+    Settings.cpp \
+    -o student-portal
 
 echo "Build successful."
