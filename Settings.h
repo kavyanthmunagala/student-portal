cdf51dc3da0b10ed846e54f2631403f89f62307b
techteam-venturemond <techteam@ventuemond.com>
2026-08-16 23:30:19 +0530

    Add student portal settings component

---
+++ Settings.h
@@ -0,0 +1,12 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <string>
+
+class Settings {
+public:
+    static std::string defaultTheme();
+    static bool notificationsEnabled();
+};
+
+#endif
