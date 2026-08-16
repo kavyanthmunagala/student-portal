550b5f4867773eff821c52491cb9ec1c6eb728cc
techteam-venturemond <techteam@ventuemond.com>
2026-08-17 00:04:52 +0530

    Clean executable before building

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+rm -f student-portal
+
 g++ -std=c++17 main.cpp StudentManager.cpp StudentSearch.cpp AttendanceValidator.cpp Login.cpp Profile.cpp Dashboard.cpp Settings.cpp -o student-portal
 
 echo "Build successful."
